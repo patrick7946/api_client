0f0a03c8dcff0cd028abdf1e2d38016df025c6f4
patrick <patrick@ibebu.net>
2023-05-09 20:17:43 +0900

    feat : 버전 조회 API
    버전 조회 API 및 테스트 코드 구현
    
    XODH-5

--- src/main/kotlin/net/ibebu/user/config/SwaggerConfig.kt
+++ src/main/kotlin/net/ibebu/user/config/SwaggerConfig.kt
@@ -13,19 +13,18 @@ import java.util.*
 @Configuration
 class SwaggerConfig {
     @Bean
-    fun openAPI(@Value("\${springdoc.version}") springdocVersion: String?): OpenAPI? {
+    fun openAPI(
+        @Value("\${springdoc.version}") springdocVersion: String?,
+        @Value("\${springdoc.server_url}") serverUrl: String
+    ): OpenAPI? {
         val info: Info = Info()
             .title("타이틀 입력")
             .version(springdocVersion)
             .description("API에 대한 설명 부분")
 
-        val localServer = Server().url("http://localhost:3002").description("for local usages")
-        val devServer = Server().url("https://dev.ibebu.net:3002").description("for testing")
-
         return OpenAPI()
             .components(Components())
             .info(info)
-            .addServersItem(localServer)
-            .addServersItem(devServer)
+            .addServersItem(Server().url(serverUrl))
     }
 }
