0f3dd82be28268b4e43c87ae42945d787b6bfd4e
patrick <patrick@ibebu.net>
2023-05-09 15:10:14 +0900

    init

---
+++ src/test/kotlin/net/ibebu/user/UserApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.ibebu.user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
