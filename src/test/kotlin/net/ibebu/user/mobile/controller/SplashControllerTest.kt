1a28ebeb70b341f4b342e5bde0b038bbd6012a28
patrick <patrick@ibebu.net>
2023-05-10 20:46:36 +0900

    feat : API 토큰 암호화 처리
    JWT 를 사용한 토큰 암호화 처리
    
    XODH-7

--- src/test/kotlin/net/ibebu/user/mobile/controller/SplashControllerTest.kt
+++ src/test/kotlin/net/ibebu/user/mobile/controller/SplashControllerTest.kt
@@ -1,7 +1,6 @@
 package net.ibebu.user.mobile.controller
 
 import net.ibebu.user.core.BaseControllerTest
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -11,13 +10,25 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @AutoConfigureMockMvc
 @SpringBootTest
-internal class SplashControllerTest : BaseControllerTest(){
+internal class SplashControllerTest : BaseControllerTest() {
     @Test
-    fun `버전 조회`(){
+    fun `버전 조회`() {
         mockMvc.perform(
             MockMvcRequestBuilders.get("/v1/splash/version")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .param("deviceType","A")
+                .param("deviceType", "A")
+        ).andExpect(MockMvcResultMatchers.jsonPath("\$.responseCode").value(SUCCESS_CODE))
+    }
+
+    @Test
+    fun `로그인 유효성 검증 - 성공`() {
+        val loginData = mapOf("email" to "qk54r71@nate.com", "password" to "ibebu794613")
+        val jsonRequest = objectMapper.writeValueAsString(loginData)
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/v1/splash/login")
+                .content(jsonRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
         ).andExpect(MockMvcResultMatchers.jsonPath("\$.responseCode").value(SUCCESS_CODE))
     }
 }
